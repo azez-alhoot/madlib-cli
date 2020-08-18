94c5eb48a9ccb32758cc8ac4d593531aeee26e31
azez-alhoot <azezalhoot82@yahoo.com>
2020-08-19 00:56:15 +0300

    lab3 not completed

---
+++ madlib_cli/madlib_cli.py
@@ -0,0 +1,45 @@
+import re
+print("""
+
+Put words as called for in the spaces and create a story by complete
+entring the values to create a story like Mad Libs it's fun. 
+What you've created may end up being fantastic, screamingly funny, shocking, silly, crazy 
+or just plain dumb. 
+It all depends upon the words you've chosen and how they "fit" into the Madlibs story!
+
+""")
+
+
+lst = []
+def  read_template():
+    with open("assets/madlip_cli.txt",'r') as f :
+        constant = f.read()
+        res = re.findall(r'\{.*?\}', constant)
+        for i in res:
+            lst.append(i.strip("{ }"))
+
+read_template()
+
+def parse():
+    with open("assets/madlip_cli.txt",'r') as f :
+        constant = f.read()
+        res = re.sub(r'\{.*?\}' , '{}' , constant )
+
+        return res
+parse()
+
+ans = []
+for i in lst:
+    a = input(f'inter a {i}:')
+    ans.append(a)
+
+def merge():
+    constant = parse()
+    f2 =open("assets/madlip_cli_copy.txt",'+w')
+    const2 = f2.write(constant.format(*ans))
+    return const2
+merge()
+
+f3 =open("assets/madlip_cli_copy.txt",'r')
+const3 = f3.read()
+print(const3)
